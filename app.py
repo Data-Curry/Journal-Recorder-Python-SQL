c714eccf5e27c25394cfe21e74bd0f73ecd82421
Christopher Austin <66553323+Data-Curry@users.noreply.github.com>
2023-02-03 19:11:42 +0900

    Add basic functions.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import add_entry, get_entries
+from database import create_table, add_entry, get_entries
 
 menu = """Please select one of the following options:
 1) Add a new entry for today.
@@ -18,10 +18,11 @@ def prompt_new_entry():
 
 def view_entries(entries):
     for entry in entries:
-        print(f"{entry['date']}\n{entry['content']}\n\n")
+        print(f"{entry[1]}\n{entry[0]}\n\n")
 
 
 print(welcome)
+create_table()
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":

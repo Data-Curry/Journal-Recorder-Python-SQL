80fef16619516edc2468728d9fc55d78d6d4680b
Christopher Austin <66553323+Data-Curry@users.noreply.github.com>
2024-01-20 21:58:06 +0900

    Add db column: category, add functions: add category, view categories, view entries by category, view titles by category, view one category's titles by a certain date, view one category's entries by a certain date

--- app.py
+++ app.py
@@ -1,55 +1,113 @@
-from database import create_table, add_entry, get_entries, get_entry, get_titles, get_entries_of_date, get_titles_of_date
+from database import create_tables, add_entry, add_category, get_entries, get_entry, get_titles, get_entries_of_date, \
+    get_titles_of_date, get_categories, get_category_titles, get_category, get_category_entries, \
+    get_a_categorys_titles_of_date, get_a_categorys_entries_of_date
 import datetime
 
 menu = """Please select one of the following options:
-1) Add a new entry.
-2) View all entry titles.
-3) View all entry titles of a certain date.
-4) View an entry by id.
-5) View all entries of a certain date.
+1) Add an entry.
+2) Add a category.
+3) View an entry by id.
+4) View all categories.
+5) View all titles.
 6) View all entries.
-7) Exit.
+
+   ** Viewing by category **
+7) View a category's titles.
+8) View a category's entries.
+
+    ** Viewing by date **
+9) View all titles of a certain date.
+10) View all entries of a certain date
+11) View a category's titles of a certain date.
+12) View a category's entries of a certain date.
+13) Exit.
 
 Your selection: """
 welcome = "Welcome to the journal recorder."
 
 
-def prompt_new_entry():
+def prompt_new_entry():                                                              # Adds a new entry to the database
+    entry_category = input("What category is this entry for? Enter category id: ")
     entry_title = input("What is the entry title? ")
     entry_content = input("What is the entry? ")
     entry_date = datetime.datetime.today().strftime("%Y-%m-%d")
     entry_time = datetime.datetime.today().strftime("%H:%M:%S")
 
-    add_entry(entry_title, entry_content, entry_date, entry_time)
+    add_entry(entry_title, entry_content, entry_date, entry_time, entry_category)
 
 
-def view_entries(entries):
+def prompt_new_category():                                                        # Adds a new category to the database
+    category_name = input("What is the category name? ")
+    add_category(category_name)
+
+
+def view_categories():                                                            # Prints all categories and their ids
+    received_list = get_categories()
+    print("All categories:")
+    for category_tuple in received_list:
+        print(f"Category #{category_tuple[0]}: {category_tuple[2]}")
+    print("\n")
+
+
+def view_entries():                                                              # Prints all entries of all categories
+    categories = get_categories()
+    entries = get_entries()
+    print("All entries...")
     for entry in entries:
-        print(f"Entry #{entry[0]}\n{entry[3]} at {entry[4]}\n{entry[1]}\n{entry[2]}\n")
+        for i in range(len(categories)):
+            if entry[6] == categories[i][0]:
+                cat_name = categories[i][2]
+                print(f"Entry #{entry[0]} {entry[4]} at {entry[5]} Category #{entry[6]}: {cat_name}\n"
+                      f"Title: {entry[2]}\n{entry[3]}\n")
 
 
-def view_entry(entry):
-    putout = entry[0]
-    print(f"Entry #{entry_id}\n{putout[2]} at {putout[3]}\n{putout[0]}\n{putout[1]}\n\n")
+def view_entry(entry):                                                                          # Prints a single entry
+    output = entry[0]  # extracts the tuple from the list
+    print(f"Entry #{output[0]} of Category #{output[6]} | {output[4]} at {output[5]}\n"
+          f"Title: {output[2]}\n{output[3]}\n")
 
 
-def view_entry_titles(entry_titles):
+def view_entry_titles():                                     # Prints all entry titles of all categories with their ids
+    entry_titles = get_titles()
+    print("All entry titles:")
     for entry in entry_titles:
         print(f"{entry[0]}: {entry[1]}")
     print("\n")
 
 
-def prompt_search_date():
+def view_category_titles():                                      # Prints all entry titles of a category with their ids
+    category_id = input("Enter the category id: ")
+    category_name = get_category(category_id)[0][2]
+    category_titles = get_category_titles(category_id)
+
+    print(f"All titles of category {category_id}: {category_name}")
+    for entry in category_titles:
+        print(f"{entry[0]}: {entry[1]}")
+    print("\n")
+
+
+def view_category_entries():                                          # Prints all entries of a category with their ids
+    category_id = input("Enter the category id: ")
+    category_name = get_category(category_id)[0][2]
+    category_entries = get_category_entries(category_id)
+
+    print(f"All entries of category {category_id}: {category_name}")
+    for entry in category_entries:
+        print(f"Entry #{entry[0]} {entry[4]} at {entry[5]}\nTitle: {entry[2]}\n{entry[3]}\n")
+    print("\n")
+
+
+def prompt_search_date():                              # Prints all entries of all categories entered on a certain date
     entry_date = input("Enter the date you would like to search for (YYYY-MM-DD): ")
     entries_of_date = get_entries_of_date(entry_date)
     print(f"All entries of {entry_date}:")
     for entry in entries_of_date:
-        # print(entry)
-        print(f"{entry[3]}: {entry[0]}\n{entry[1]}")
+        cat_name = get_category(entry[6])[0][2]
+        print(f"{entry[5]} | entry #{entry[0]}: {entry[2]} | category #{entry[6]}: {cat_name}\n{entry[3]}")
     print("\n")
 
 
-def prompt_search_titles_of_date():
+def prompt_search_titles_of_date():               # Prints all entry titles of all categories entered on a certain date
     entry_date = input("Enter the date you would like to search for (YYYY-MM-DD): ")
     entries_of_date = get_titles_of_date(entry_date)
     print(f"All titles of {entry_date}:")
@@ -58,26 +116,59 @@ def prompt_search_titles_of_date():
     print("\n")
 
 
-print(welcome)
-create_table()
+def prompt_search_a_categorys_titles_of_date():       # Prints all entry titles of a category entered on a certain date
+    category_id = input("Enter the category id: ")
+    entry_date = input("Enter the date you would like to search for (YYYY-MM-DD): ")
+    cat_name = get_category(category_id)[0][2]
+    entries_of_date = get_a_categorys_titles_of_date(category_id, entry_date)
+    print(f"All titles of {entry_date} in category {category_id}: {cat_name}")
+    for entry in entries_of_date:
+        print(f"{entry[0]}: {entry[1]}")
+    print("\n")
 
-while (user_input := input(menu)) != "7":
+
+def prompt_search_a_categorys_entries_of_date():           # Prints all entries of a category entered on a certain date
+    category_id = input("Enter the category id: ")
+    entry_date = input("Enter the date you would like to search for (YYYY-MM-DD): ")
+    cat_name = get_category(category_id)[0][2]
+    entries_of_date = get_a_categorys_entries_of_date(category_id, entry_date)
+    print(f"All entries of {entry_date} in category {category_id}: {cat_name}")
+    for entry in entries_of_date:
+        print(f"Entry #{entry[0]} {entry[4]} at {entry[5]}\nTitle: {entry[2]}\n{entry[3]}\n")
+    print("\n")
+
+
+print(welcome)
+create_tables()
+
+while (user_input := input(menu)) != "13":                                                         # The main menu loop
     if user_input == "1":
         prompt_new_entry()
     elif user_input == "2":
-        view_entry_titles(get_titles())
+        prompt_new_category()
     elif user_input == "3":
-        prompt_search_titles_of_date()
-    elif user_input == "4":
-        entry_id = int(input("Enter the entry id: "))
         try:
+            entry_id = input("Enter the entry id: ")
             view_entry(get_entry(entry_id))
         except:
             print("Entry not found.")
+    elif user_input == "4":
+        view_categories()
     elif user_input == "5":
-        prompt_search_date()
+        view_entry_titles()
     elif user_input == "6":
-        view_entries(get_entries())
+        view_entries()
+    elif user_input == "7":
+        view_category_titles()
+    elif user_input == "8":
+        view_category_entries()
+    elif user_input == "9":
+        prompt_search_titles_of_date()
+    elif user_input == "10":
+        prompt_search_date()
+    elif user_input == "11":
+        prompt_search_a_categorys_titles_of_date()
+    elif user_input == "12":
+        prompt_search_a_categorys_entries_of_date()
     else:
         print("Invalid input.  Please try again.")
-
--- database.py
+++ database.py
@@ -1,60 +1,115 @@
 import sqlite3
 from typing import Tuple, List
 
-Entry = Tuple[int, str, str, str, str]
+Entry = Tuple[str, str, str, str, str, str]
 
+CREATE_CATEGORIES = """CREATE TABLE IF NOT EXISTS categories (id SERIAL PRIMARY KEY, name TEXT);"""
 CREATE_ENTRIES = """CREATE TABLE IF NOT EXISTS entries
-(title TEXT, content TEXT, date TEXT, time TEXT);"""
+(id SERIAL PRIMARY KEY, title TEXT, content TEXT, date TEXT, time TEXT, category_id INTEGER, FOREIGN KEY(category_id) 
+REFERENCES categories (rowid));"""
 
-INSERT_ENTRY = "INSERT INTO entries (title, content, date, time) VALUES (?, ?, ?, ?) RETURNING ROWID;"
+INSERT_CATEGORY = "INSERT INTO categories (name) VALUES (?) RETURNING ROWID;"
+INSERT_ENTRY = "INSERT INTO entries (title, content, date, time, category_id) VALUES (?, ?, ?, ?, ?) RETURNING ROWID;"
 
+SELECT_ALL_CATEGORIES = "SELECT rowid, * FROM categories;"
 SELECT_ALL_ENTRIES = "SELECT rowid, * FROM entries;"
-SELECT_ENTRY = "SELECT * FROM entries WHERE rowid = ?;"
-SELECT_ENTRIES_OF_DATE = "SELECT * FROM entries WHERE date = ?;"
+
+SELECT_CATEGORY = "SELECT rowid, * FROM categories WHERE rowid = ?;"
+SELECT_ENTRY = "SELECT rowid, * FROM entries WHERE rowid = ?;"
+SELECT_ENTRIES_OF_DATE = "SELECT rowid, * FROM entries WHERE date = ?;"
+SELECT_A_CATEGORYS_ENTRIES_OF_DATE = "SELECT rowid, * FROM entries WHERE date = ? AND category_id = ?;"
 SELECT_ENTRY_TITLES = "SELECT rowid, title FROM entries;"
 SELECT_ENTRY_TITLES_OF_DATE = "SELECT rowid, title FROM entries WHERE date = ?;"
+SELECT_A_CATEGORYS_TITLES_OF_DATE = "SELECT rowid, title FROM entries WHERE date = ? AND category_id = ?;"
+
+SELECT_CATEGORY_TITLES = "SELECT rowid, title FROM entries WHERE category_id = ?;"
+SELECT_CATEGORY_ENTRIES = "SELECT rowid, * FROM entries WHERE category_id = ?;"
 
 
 connection = sqlite3.connect("journal_data.db")
 
 
-def create_table():
+def create_tables():
     with connection:
+        connection.execute(CREATE_CATEGORIES)
         connection.execute(CREATE_ENTRIES)
 
 
-def add_entry(entry_title, entry_content, entry_date, entry_time):
+def add_category(category_name: str):
     with connection:
-        curse = connection.execute(INSERT_ENTRY, (entry_title, entry_content, entry_date, entry_time))
-        entry_id = curse.fetchone()[0]
-        print(f"Added entry with id {entry_id}")
+        cursor = connection.execute(INSERT_CATEGORY, (category_name,))
+        category_id = cursor.fetchone()[0]
+        print(f"Added category {category_name} with id {category_id}")
+
+
+def add_entry(entry_title, entry_content, entry_date, entry_time, category_id):
+    with connection:
+        cursor = connection.execute(INSERT_ENTRY, (entry_title, entry_content, entry_date, entry_time, category_id))
+        entry_id = cursor.fetchone()[0]
+        print(f"Added entry {entry_title} with id {entry_id}")
+
+
+def get_categories():
+    with connection:
+        cursor = connection.execute(SELECT_ALL_CATEGORIES)
+        return cursor.fetchall()
 
 
 def get_entries():
     with connection:
-        curse = connection.execute(SELECT_ALL_ENTRIES)
-        return curse.fetchall()
+        cursor = connection.execute(SELECT_ALL_ENTRIES)
+        return cursor.fetchall()
 
 
-def get_entry(entry_id: int) -> List[Entry]:
+def get_category(category_id: str):
     with connection:
-        curse = connection.execute(SELECT_ENTRY, (entry_id,))
-        return curse.fetchall()
+        cursor = connection.execute(SELECT_CATEGORY, (category_id,))
+        return cursor.fetchall()
+
+
+def get_entry(entry_id: str):
+    with connection:
+        cursor = connection.execute(SELECT_ENTRY, (entry_id,))
+        return cursor.fetchall()
 
 
 def get_titles():
     with connection:
-        curse = connection.execute(SELECT_ENTRY_TITLES)
-        return curse.fetchall()
+        cursor = connection.execute(SELECT_ENTRY_TITLES)
+        return cursor.fetchall()
+
+
+def get_category_titles(category_id: str):
+    with connection:
+        cursor = connection.execute(SELECT_CATEGORY_TITLES, (category_id,))
+        return cursor.fetchall()
+
+
+def get_category_entries(category_id: str):
+    with connection:
+        cursor = connection.execute(SELECT_CATEGORY_ENTRIES, (category_id,))
+        return cursor.fetchall()
 
 
 def get_entries_of_date(entry_date: str) -> List[Entry]:
     with connection:
-        curse = connection.execute(SELECT_ENTRIES_OF_DATE, (entry_date,))
-        return curse.fetchall()
+        cursor = connection.execute(SELECT_ENTRIES_OF_DATE, (entry_date,))
+        return cursor.fetchall()
 
 
 def get_titles_of_date(entry_date: str) -> List[Entry]:
     with connection:
-        curse = connection.execute(SELECT_ENTRY_TITLES_OF_DATE, (entry_date,))
-        return curse.fetchall()
+        cursor = connection.execute(SELECT_ENTRY_TITLES_OF_DATE, (entry_date,))
+        return cursor.fetchall()
+
+
+def get_a_categorys_titles_of_date(category_id: str, entry_date: str) -> List[Entry]:
+    with connection:
+        cursor = connection.execute(SELECT_A_CATEGORYS_TITLES_OF_DATE, (entry_date, category_id))
+        return cursor.fetchall()
+
+
+def get_a_categorys_entries_of_date(category_id: str, entry_date: str) -> List[Entry]:
+    with connection:
+        cursor = connection.execute(SELECT_A_CATEGORYS_ENTRIES_OF_DATE, (entry_date, category_id))
+        return cursor.fetchall()
